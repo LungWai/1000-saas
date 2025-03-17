b89a59263ec009cf80f8d096feec6367958343d6
LungWai <lungw96@gmail.com>
2025-03-17 17:30:35 +0800

    Update next config

--- next.config.js
+++ next.config.js
@@ -21,6 +21,7 @@ const nextConfig = {
     parallelServerBuildTraces: true,
     parallelServerCompiles: true,
   },
+  transpilePackages: ['@vercel/analytics'],
 }
 
 mergeConfig(nextConfig, userConfig)
